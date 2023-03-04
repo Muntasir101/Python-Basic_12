ec4dc05f3b47eb126660ee823b60ae23fe2ce279
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2023-03-04 12:10:04 +0600

    variables,Operators

---
+++ 1_Variables/variables_demo.py
@@ -0,0 +1,30 @@
+"""
+A variable refers to a memory address in which data is stored.
+"""
+first_name = "superman"
+age = 40
+city = 'Dhaka'
+tax = 20.5
+status = True
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, "Dhaka", 12.22, True]
+student_info = {
+    "first_name": "Ironman",
+    "last_name": "Hero",
+    "dept": "English",
+    "roll": 123
+}
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Dhaka", 12.22, True)
+
+"""
+Data Type:
+1. string("", '')
+2. integer
+3. float(20.5)
+4. boolean(True, False)
+5. list
+6. tuples
+7. dictionaries
+"""
+
+print(type(age))
---
+++ 2_Operators/operators_demo.py
@@ -0,0 +1,43 @@
+"""
+4 Operator in python
+1. Assignment operator
+2. Arithmetic operator
+3. Logical operator
+4. Comparison operator
+"""
+
+# Assignment Operators
+number1 = 10
+#number1 += 1
+#number1 -= 1
+
+# Arithmetic Operators
+result = number1 + number1 # Addition 20
+print(result)
+result = result - number1 # Subtraction 10
+print(result)
+result = result * result # Multiplication 100
+print(result)
+result = result / result # Divide 1
+print(result)
+result = number1 % 10 # Modulo 0
+print(result)
+result = number1 // 12 # Floor Divide 0
+print(result)
+result2 = 5 ** 2 # Exponentiation 1
+print(result2)
+
+# Comparison Operators
+print(10 == 10)  # Equals
+print(10 != 10) # NotEquals
+print(10 > 20)  # Greater than
+print(10 < 20) # Lesser than
+print(10 >= 10) # Greater than or equal
+print(10 <= 20) # Lesser than or equal
+
+print(".................")
+# Logical Operators
+print(1.0 == 1.2 and 10 < 20) # Logical
+print(1.0 == 1.2 or 10 < 20)
+print(not 10 < 10)
+
